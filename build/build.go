518f6cc101cfe55b4a7f169706f8fb612ffc7f69
Kentaro Shinto <shintok@gmail.com>
2024-01-27 01:25:38 +0900

    v0.0.5

--- build/build.go
+++ build/build.go
@@ -1,5 +1,5 @@
 //go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 //go:generate go get github.com/snt/dump1090-protobuf@v0.0.3
-//go:generate protoc --proto_path=$GOPATH/pkg/mod/github.com/snt/dump1090-protobuf@v0.0.3 --go_out=../.. --go_opt=Madsb.proto=dump1090-protobuf-go/ adsb.proto
+//go:generate protoc --proto_path=$GOPATH/pkg/mod/github.com/snt/dump1090-protobuf@v0.0.3 --go_out=.. --go_opt=Madsb.proto=adsb/ adsb.proto
 
 package build
