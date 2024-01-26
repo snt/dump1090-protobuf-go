96443c1764c21732cb2af197441e145edbaaa39e
Kentaro Shinto <shintok@gmail.com>
2024-01-27 02:23:49 +0900

    moved .pb.go to root

--- build/build.go
+++ build/build.go
@@ -1,5 +1,5 @@
 //go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 //go:generate go get github.com/snt/dump1090-protobuf@v0.0.3
-//go:generate protoc --proto_path=$GOPATH/pkg/mod/github.com/snt/dump1090-protobuf@v0.0.3 --go_out=.. --go_opt=Madsb.proto=adsb/ adsb.proto
+//go:generate protoc --proto_path=$GOPATH/pkg/mod/github.com/snt/dump1090-protobuf@v0.0.3 --go_out=.. --go_opt=paths=source_relative  --go_opt=Madsb.proto=adsb/ adsb.proto
 
 package build
